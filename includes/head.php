2517b49a8fa3e032ca0e24f6c7499ef059d8e46d
FlitPix <8645405+FlitPix@users.noreply.github.com>
2025-11-06 19:49:48 -0500

    fix pages in subfolders failing to find css and favicon

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="style.css">
-<link rel="icon" type="image/x-icon" href="images/favicon.ico">
+<link rel="stylesheet" type="text/css" href="/style.css">
+<link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 <meta name="og:type" content="website">
 <meta name="og:url" content="https://flitpix.net">
 <meta name="og:site_name" content="Flit's Amazing Cool Website">
